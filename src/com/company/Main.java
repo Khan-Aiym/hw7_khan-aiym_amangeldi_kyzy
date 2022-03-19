9f7fbfa235a2a73e5347218166d3c2b8c47cd503
khan_aiym <hanaiym9@gmail.com>
2022-03-20 00:45:41 +0300

    set commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,6 @@ public class Main {
       public static void main(String[] args) {
             Hero[] Heroes = {new Magic(), new Medic(), new Warrior()};
             for (int i = 0; i < Heroes.length; i++) {
-                  System.out.println(new Magic().HavingSuperAbility("Kinetic"));
-                  System.out.println(new Medic().HavingSuperAbility("Physic"));
-                  System.out.println(new Warrior().HavingSuperAbility("Magic"));
-                  System.out.println(new Medic().increaseExperience());
+                  System.out.println(Heroes [i].HavingSuperAbility("Critical damage"));
             }
 }}
